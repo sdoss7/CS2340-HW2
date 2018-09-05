2f4cb9df7e63cee835c10e6a22b8b43ff014fcfb
skapur12 <42812319+skapur12@users.noreply.github.com>
2018-09-05 11:29:20 -0400

    Update Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,10 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      for (int i = 0; i < input.length(); i++) {
+            newString = newString + (char)(input.charAt(i) + 1);
+        }
+        return newString; 
     }
     
     /**
