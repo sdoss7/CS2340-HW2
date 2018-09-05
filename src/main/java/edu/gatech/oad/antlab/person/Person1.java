0667e412e04208c8be262d75864587ec5066f716
Samuel Doss <sdoss7@gatech.edu>
2018-09-04 23:53:04 -0400

    Update Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Samuel
  *  @version 1.1
  */
 public class Person1 {
@@ -30,8 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  return input.substring(2, input.length()) + input.substring(0,2);
 	}
 	
 	/**
